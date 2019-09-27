5ab0cb4dd778c633a20b5325ba9c7e52b2a68d1e
seamus.collins <seamus.collins@PC071-L118-06.boone.ketsds.net>
2019-09-27 13:39:15 -0400

    test

---
+++ CharInfo/src/main/AppletRunner.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.awt.Frame;
+import java.awt.event.ActionListener;
+import java.applet.Applet;
+import java.awt.Button;
+
+public class AppletRunner extends Applet{
+	public AppletRunner() {
+		/*Frame f=new Frame("Button Example");  
+		Button b=new Button("Races");  
+		b.setBounds(50,100,80,30);  
+		f.add(b);  
+		f.setSize(400,400);  
+		f.setLayout(null);  
+		f.setVisible(true);
+		Frame racePage = new Frame("Races");
+		racePage.setSize(400,400);
+		f.setLayout(null);
+		racePage.setVisible(true);*/
+		
+		Frame home = new Frame("Homepage");
+		home.setSize(1375, 800);
+		home.setVisible(true);
+		Button raceButton = new Button("Races");
+
+		
+		 }
+		 
+
+		
+	}
