3e6d83eb37a6973c529bfc3b9fa776ee846328d1
seamus.collins <seamus.collins@PC071-L118-06.boone.ketsds.net>
2019-10-01 13:17:14 -0400

    hi

--- CharInfo/src/main/AppletRunner.java
+++ CharInfo/src/main/AppletRunner.java
@@ -1,11 +1,20 @@
 package main;
 
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.applet.Applet;
 import java.awt.Button;
+import java.awt.GridLayout;
 
-public class AppletRunner extends Applet{
+public class AppletRunner extends Applet implements ActionListener{
+	Frame homeFrame = new Frame("Homepage");
+	Frame raceFrame = new Frame("Races");
+	Frame classFrame = new Frame("Classes");
+	Frame backgroundFrame = new Frame("Backgrounds");
+	Button raceButton = new Button("Races");
+	Button classButton = new Button("Classes");
+	Button backgroundButton = new Button("Backgrounds");
 	public AppletRunner() {
 		/*Frame f=new Frame("Button Example");  
 		Button b=new Button("Races");  
@@ -18,15 +27,20 @@ public class AppletRunner extends Applet{
 		racePage.setSize(400,400);
 		f.setLayout(null);
 		racePage.setVisible(true);*/
+		turnOnFrame(homeFrame);
+		turnOnFrame(raceFrame);
+		turnOnFrame(classFrame);
+		turnOnFrame(backgroundFrame);
 		
-		Frame home = new Frame("Homepage");
-		home.setSize(1375, 800);
-		home.setVisible(true);
-		Button raceButton = new Button("Races");
-
 		
 		 }
-		 
-
-		
+	public void actionPerformed(ActionEvent arg0) {
+		homeFrame.setVisible(false);
+		races.setVisible(true);
 	}
+	public void turnOnFrame(Frame f) {
+		f.setLayout(null);
+		f.setSize(1000, 1000);
+		f.setVisible(true);
+	}
+}
