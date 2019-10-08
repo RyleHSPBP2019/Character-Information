30df53981f0b840d9deed2b9007d1642e79898c9
cody.osborne <cody.osborne@PC071-L118-07.boone.ketsds.net>
2019-10-08 13:30:51 -0400

    push

--- CharInfo/src/races/Races.java
+++ CharInfo/src/races/Races.java
@@ -19,7 +19,11 @@ public class Races {
 		String extra2 = new String();
 		String extra3 = new String();
 		String extra4 = new String();
+		String extra5 = new String();
+		String extra6 = new String ();
 		String languages = new String();
+		String cantrips = new String ();
+		
 		boolean vision = false;
 		
 		//Sets Base Race
@@ -40,7 +44,18 @@ public class Races {
 			languages = "You can read, speak, and write in common and dwarvish";
 			extra = "You have advantages on saving rolls against poison, you are also resistance agaisnt poison";
 			extra2 = "You have proficiency in battleaxes, handaxe, lighhammer. you also have proficiency in artisan's tools such as smith’s tools, brewer’s supplies, or mason’s tools.";
-			extra3 = "you can make a intell(history) check on stonework to see when it was made";		
+			extra3 = "You can make a intell(history) check on stonework to see when it was made";		
+		case elf:
+			age = "100-750";
+			align = "Generally Chaotic Good";
+			size = "Genreally 5ft - 6'5ft";
+			vision = true;
+			extra = "Proficiency in the Perception skill";
+			extra2 = "advantage on saving throws against being charmed, and magic can’t put you to sleep";
+			extra3 = "Elves don’t need to sleep. Instead, they meditate deeply";
+		case gnome:
+			extra ="You have advantage on all Intelligence, Wisdom, and Charisma saving throws against magic";
+					
 		}
 		//Sets Sub Race
 		switch(subType) {
@@ -82,6 +97,19 @@ public class Races {
 			extra4 = " proficiency with light and medium armor";
 			break;
 			
+		case high:
+			asi = "Dexterity + 2 and Intelligence + 1";
+			speed = "30ft";
+			extra4 = "Proficiency with the longsword, shortsword, shortbow, and longbow";
+			cantrips = "You know one cantrip of your choice from the wizard spell list. Intelligence is your spellcasting ability for it";
+			languages = "You can read, speak, and write in common and elvish + 1 extra language of your choice";
+			break;
+		case wood:
+			asi = "Dexterity + 2 and Wisdom + 1";
+			speed = "35ft";
+			extra4 = "proficiency with the longsword, shortsword, shortbow, and longbow";
+			extra5 = "You can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena";
+			languages = "You can read, speak, and write in common and elvish";
 		case nun:
 			break;
 		}
