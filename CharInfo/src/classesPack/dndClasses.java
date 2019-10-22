b45af000ca5b5c326adf8c6f1d28c27fd72df53e
cody.osborne <cody.osborne@PC071-L118-07.boone.ketsds.net>
2019-10-22 12:59:27 -0400

    thats desgusting smee

--- CharInfo/src/classesPack/dndClasses.java
+++ CharInfo/src/classesPack/dndClasses.java
@@ -4,49 +4,72 @@ public class dndClasses {
 	public classPoss classType;
 	public archPoss archType;
 	public warlockPact pactType;
-	public String asi = "increase one ability score by 2 or increase two ability scores by 1 ";
-	public String twentyAbt1 = new String();
-	public String nineteenAbt1 = new String();
-	public String eightteenAbt1 = new String();
-	public String seventeenAbt1 = new String();
-	public String sixteenAbt1 = new String();
-	public String fifthteenAbt1 = new String();
-	public String fourteenAbt1 = new String();
-	public String thirteenAbt1 = new String();
-	public String twelveAbt1 = new String();
-	public String elevenAbt1 = new String();
+	public  String asi = "increase one ability score by 2 or increase two ability scores by 1 ";
+	public  String twentyAbt1 = new String();
+	public  String twentyAbt2 = new String();
+	public  String twentyAbt3 = new String();
+	public  String nineteenAbt1 = new String();
+	public  String nineteenAbt2 = new String();
+	public  String nineteenAbt3 = new String();
+	public  String eighteenAbt1 = new String();
+	public  String eighteenAbt2 = new String();
+	public  String eighteenAbt3 = new String();
+	public  String seventeenAbt1 = new String();
+	public  String seventeenAbt2 = new String();
+	public  String seventeenAbt3 = new String();
+	public  String sixteenAbt1 = new String();
+	public  String sixteenAbt2 = new String();
+	public  String sixteenAbt3 = new String();
+	public  String fifteenAbt1 = new String();
+	public  String fifteenAbt2 = new String();
+	public  String fifteenAbt3 = new String();
+	public  String fourteenAbt1 = new String();
+	public  String fourteenAbt2 = new String();
+	public  String fourteenAbt3 = new String();
+	public  String fourteenArch = new String();
+	public  String thirteenAbt1 = new String();
+	public  String thirteenAbt2 = new String();
+	public  String thirteenAbt3 = new String();
+	public  String twelveAbt1 = new String();
+	public  String twelveAbt2 = new String();
+	public  String twelveAbt3 = new String();
+	public  String elevenAbt1 = new String();
+	public  String elevenAbt2 = new String();
+	public  String elevenAbt3 = new String();
 	public 	String tenAbt1 = new String();
-	public String nineAbt1 = new String();
+	public 	String tenAbt2 = new String();
+	public 	String tenAbt3 = new String();
+	public 	String tenArch = new String();
+	public  String nineAbt1 = new String();
+	public  String nineAbt2 = new String();
+	public  String nineAbt3 = new String();
 	public 	String eightAbt1 = new String();
+	public 	String eightAbt2 = new String();
+	public 	String eightAbt3 = new String();
 	public 	String sevenAbt1 = new String();
+	public 	String sevenAbt2 = new String();
+	public 	String sevenAbt3 = new String();
 	public 	String sixAbt1 = new String();
 	public 	String sixAbt2 = new String();
 	public 	String sixAbt3 = new String();
-	public 	String sixAbt4 = new String ();
-	public String fiveAbt1 = new String();
+	public  String sixArch = new String();
+	public  String fiveAbt1 = new String();
 	public 	String fiveAbt2 = new String();
+	public 	String fiveAbt3 = new String();
 	public 	String fourAbt1 = new String();
+	public 	String fourAbt2 = new String();
+	public 	String fourAbt3 = new String();
 	public 	String threeAbt1 = new String();
 	public 	String threeAbt2 = new String();
-	public 	String threeAbt3 = new String();
-	public 	String threeAbt4 = new String();
+	public 	String threeAbt3 = new String();	
+	public  String threeArch = new String();
 	public 	String twoAbt1 = new String();
 	public 	String twoAbt2 = new String();
+	public  String twoAbt3 = new String();
 	public 	String oneAbt1 = new String();
 	public 	String oneAbt2 = new String();
-	public 	String feat = new String();
-	public 	String feat2 = new String();
-	public 	String feat3 = new String();
-	public 	String feat4 = new String();
-	public 	String bear1 = new String();
-	public 	String bear2 = new String();
-	public 	String bear3 = new String();
-	public 	String eagle1 = new String();
-	public 	String eagle2 = new String();
-	public 	String eagle3 = new String();
-	public 	String wolf1 = new String();
-	public 	String wolf2 = new String();
-	public 	String wolf3 = new String();
+	public  String oneAbt3 = new String();
+
 	public 	String proficiencyBonus = new String();
 	
 	public dndClasses() {
@@ -56,7 +79,7 @@ public class dndClasses {
 	}
 	public void setClass(archPoss Archtype, classPoss ClassType, warlockPact PactType, int lvl) {
 		classType = ClassType;
-		archType = Archtype;
+		archType = Archtype; 
 		pactType = PactType;
 		int level = lvl;
 		
@@ -102,13 +125,13 @@ public class dndClasses {
 			case 19:
 				nineteenAbt1 = asi;
 			case 18:
-				eightteenAbt1 = "if your strength check roll is lower than your actual strength score you can use actual score instead";
+				eighteenAbt1 = "if your strength check roll is lower than your actual strength score you can use actual score instead";
 			case 17:
 				seventeenAbt1 = "roll three additional weapon damage dice after every crit";
 			case 16:
 				sixteenAbt1 = asi;
 			case 15:
-				fifthteenAbt1 = "rage is so fierce that it ends early only if you fall unconscious or if you choose to end it";
+				fifteenAbt1 = "rage is so fierce that it ends early only if you fall unconscious or if you choose to end it";
 			case 14:
 			case 13:
 				thirteenAbt1 = "roll two additional weapon damage dice after every crit";
@@ -130,6 +153,7 @@ public class dndClasses {
 			case 4:
 				fourAbt1 = asi;
 			case 3:
+				threeAbt1 = threeArch;
 			case 2:
 				twoAbt1 = "you can throw aside all concern for defense to attack with fierce desperation";
 				twoAbt2 = "you gain an uncanny sense of when things nearby aren’t as they should be, giving you an edge when you dodge away from danger";
@@ -141,18 +165,18 @@ public class dndClasses {
 			switch(archType) {
 			
 			case berserker:
-				threeAbt1 = "you can go into a frenzy when you rage, your rage you can make a single melee weapon attack as a bonus action on each of your turns after this one";
-				sixAbt1 = "you can’t be charmed or frightened while raging";
-				tenAbt1 = "you can use your action to frighten someone, choose one creature that you can see within 30 feet.it must succeed on a Wisdom saving throw (DC equal to 8 + your proficiency bonus + your Charisma modifier) or be frightened of you until the end of your next turn.";
-				fourteenAbt1 = "when you take damage from a creature that is within 5 feet of you, you can use your reaction to make a melee weapon attack against that creature";
+				threeArch = "you can go into a frenzy when you rage, your rage you can make a single melee weapon attack as a bonus action on each of your turns after this one";
+				sixArch = "you can’t be charmed or frightened while raging";
+				tenArch = "you can use your action to frighten someone, choose one creature that you can see within 30 feet.it must succeed on a Wisdom saving throw (DC equal to 8 + your proficiency bonus + your Charisma modifier) or be frightened of you until the end of your next turn.";
+				fourteenArch = "when you take damage from a creature that is within 5 feet of you, you can use your reaction to make a melee weapon attack against that creature";
 				break;
 				
 			
 			case totem:
-				threeAbt1 = "Bear: Resistnt to all damage except psychic while raging. Eagle: If NO heavy armor, disadv. on oppurtunity attacks against you. Wolf: Allies have adv. on melee attacks against creatures within 5 ft of you.";
-				sixAbt1 = "Bear: Carry Capacity doubled, adv. on Str Checks made to interact with objects. Eagle: See up to one mile with no difficulty, dim light doesn't affect you. Wolf: Can track at normal pace.";
-				tenAbt1 = "Can cast Commune with Nature as a ritual only.";
-				fourteenAbt1 = "Bear: While raging, Creatures have Disadv. on attack rolls within 5 ft of you. Eagle: While raging, gain fly speed = walking speed. Wolf: While raging can use bonus action to know Large or smaller creature prone.";
+				threeArch = "Bear: Resistnt to all damage except psychic while raging. Eagle: If NO heavy armor, disadv. on oppurtunity attacks against you. Wolf: Allies have adv. on melee attacks against creatures within 5 ft of you.";
+				sixArch = "Bear: Carry Capacity doubled, adv. on Str Checks made to interact with objects. Eagle: See up to one mile with no difficulty, dim light doesn't affect you. Wolf: Can track at normal pace.";
+				tenArch = "Can cast Commune with Nature as a ritual only.";
+				fourteenArch = "Bear: While raging, Creatures have Disadv. on attack rolls within 5 ft of you. Eagle: While raging, gain fly speed = walking speed. Wolf: While raging can use bonus action to know Large or smaller creature prone.";
 				break;
 			}
 			break;
