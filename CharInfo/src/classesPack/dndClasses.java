966faf175dcd08e571ad7ba963ce3e94823d0125
cody.osborne <cody.osborne@PC071-L118-07.boone.ketsds.net>
2019-10-24 13:15:23 -0400

    Barabaianasbas is completelo

--- CharInfo/src/classesPack/dndClasses.java
+++ CharInfo/src/classesPack/dndClasses.java
@@ -123,22 +123,22 @@ public class dndClasses {
 		case barbarian:
 			switch(level) {
 			case 20:
-				abt[39] = "Strength and Constitution + 4 and maximum cap for those skills increase to 24";
+				abt[39] = "level 20: Strength and Constitution + 4 and maximum cap for those skills increase to 24, you have upgraded to have unlimted rages per day ";
 				abt[40] = "";
 			case 19:
 				abt[37] = asi;
 				abt[38] = "";
 			case 18:
-				abt[35] = "if your strength check roll is lower than your actual strength score you can use actual score instead";
+				abt[35] = "Level 18: if your strength check roll is lower than your actual strength score you can use actual score instead";
 				abt[36] = "";
 			case 17:
-				abt[33] = "roll three additional weapon damage dice after every crit";
+				abt[33] = "Level 17: roll three additional weapon damage dice after every crit, you know gain 6 rages per day";
 				abt[34] = "";
 			case 16:
 				abt[31] = asi;
-				abt[32] = "";
+				abt[32] = "Level 16: your rage now adds +4 to your attacks";
 			case 15:
-				abt[29] = "rage is so fierce that it ends early only if you fall unconscious or if you choose to end it";
+				abt[29] = "Level 15: rage is so fierce that it ends early only if you fall unconscious or if you choose to end it";
 				abt[30] = "";
 			case 14:
 				abt[28] = "";
@@ -147,14 +147,14 @@ public class dndClasses {
 				abt[26] = "";
 			case 12:
 				abt[23] = asi;
-				abt[24] = "";
+				abt[24] = "Level 12: You now have 5 rages per day";
 			case 11:
-				abt[21] = "Level 11: your rage can keep you fighting despite grievous wounds";
+				abt[21] = "Level 11: While your raging if you drop to 0 hp you must make a Con. saving throw DC 10 (+5 number of times you have already used this skill) success 1hp else dead, resets after long rest";
 				abt[22] = "";
 			case 10:
-				abt[20] = tenArch;
+				abt[20] = "";
 			case 9:
-				abt[17] = "Level 9: roll one additional weapon damage dice after every crit";
+				abt[17] = "Level 9: roll one additional weapon damage dice after every crit, your rage now adds +3 to your attacks ";
 				abt[18] = "";
 			case 8:
 				abt[15] = asi;
@@ -173,29 +173,29 @@ public class dndClasses {
 			case 3:
 				abt[5] = "";
 			case 2:
-				abt[2] = "Level 2: you can choose to gain advantage ofr all attacks for your turn, enemies also gain advantage on attacks against you.";
-				abt[3] = "Level 2: you gain an uncanny sense of when things nearby aren’t as they should be, giving you an edge when you dodge away from danger.";
+				abt[2] = "Level 2: you can choose to gain advantage on all attacks for your turn, enemies also gain advantage on attacks against you";
+				abt[3] = "Level 2: adv. on Dex. saving throws against effects that you can see";
 			case 1:
-				abt[0] = "Level 1: while raging You have adv on Strength checks and Strength saving throws, you make a melee weapon attack using Strength, You have resistance to bludgeoning, piercing, and slashing damage";
-				abt[1] = "Level 1: While you are not wearing any armor, your Armor Class equals 10 + your Dexterity modifier + your Constitution modifier. You can use a shield and still gain this benefit";
+				abt[0] = "Level 1: while raging You have adv on Strength checks and Strength saving throws, you make a melee weapon attack using Strength, You have resistance to bludgeoning, piercing, and slashing damage, your rage adds +2 to your attacks";
+				abt[1] = "Level 1: While you are not wearing any armor, your Armor Class equals 10 + your Dexterity modifier + your Constitution modifier. You can use a shield and still gain this benefit. You get 2 rages a day";
 				name = "Barbarian";
 				break;
 			}
 			switch(archType) {
 			
 			case berserker:
-				abt[4] = "you can go into a frenzy when you rage, your rage you can make a single melee weapon attack as a bonus action on each of your turns after this one";
-				abt[11] = "you can’t be charmed or frightened while raging";
-				abt[19] = "you can use your action to frighten someone, choose one creature that you can see within 30 feet.it must succeed on a Wisdom saving throw (DC equal to 8 + your proficiency bonus + your Charisma modifier) or be frightened of you until the end of your next turn.";
+				abt[4] = "Level 3: you can go into a frenzy when you rage, your rage you can make a single melee weapon attack as a bonus action on each of your turns after this one, you have upgraded to 3 rages a day";
+				abt[11] = "Level 6: you can’t be charmed or frightened while raging, you have upgraded to 4 rages a day ";
+				abt[19] = "Level 10: you can use your action to frighten someone, choose one creature that you can see within 30 feet.it must succeed on a Wisdom saving throw (DC equal to 8 + your proficiency bonus + your Charisma modifier) or be frightened of you until the end of your next turn.";
 				abt[27] = "Level 14: when you take damage from a creature that is within 5 feet of you, you can use your reaction to make a melee weapon attack against that creature";
 				break;
 				
 			
 			case totem:
-				abt[4] = "Bear: Resistnt to all damage except psychic while raging. Eagle: If NO heavy armor, disadv. on oppurtunity attacks against you. Wolf: Allies have adv. on melee attacks against creatures within 5 ft of you.";
-				abt[11] = "Bear: Carry Capacity doubled, adv. on Str Checks made to interact with objects. Eagle: See up to one mile with no difficulty, dim light doesn't affect you. Wolf: Can track at normal pace.";
-				abt[19] = "Can cast Commune with Nature as a ritual only.";
-				abt[27] = "Bear: While raging, Creatures have Disadv. on attack rolls within 5 ft of you. Eagle: While raging, gain fly speed = walking speed. Wolf: While raging can use bonus action to know Large or smaller creature prone.";
+				abt[4] = "Level 3: Bear: Resistnt to all damage except psychic while raging. Eagle: If NO heavy armor, disadv. on oppurtunity attacks against you. Wolf: Allies have adv. on melee attacks against creatures within 5 ft of you. you've upgraded to 3 rages per day";
+				abt[11] = "Level 6: Bear: Carry Capacity doubled, adv. on Str Checks made to interact with objects. Eagle: See up to one mile with no difficulty, dim light doesn't affect you. Wolf: Can track at normal pace. You also now have 4 rages per day";
+				abt[19] = "level 10: Can cast Commune with Nature as a ritual only.";
+				abt[27] = "Level 14: Bear: While raging, Creatures have Disadv. on attack rolls within 5 ft of you. Eagle: While raging, gain fly speed = walking speed. Wolf: While raging can use bonus action to know Large or smaller creature prone.";
 				break;
 			}
 			break;
